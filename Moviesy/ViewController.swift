f0fd628288919871fe49ab61482298dfbde7e93a
Rishab Bokaria <rishab.bokaria@hungama.com>
2020-04-21 01:20:35 +0530

    Initial Commit

---
+++ Moviesy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moviesy
+//
+//  Created by Rishab on 21/04/20.
+//  Copyright © 2020 RB Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
